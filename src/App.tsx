33afd548ed0f914e33140c70dc7d3eb3b024748a
LuqmanGmeena <luqmanmeena1@gmail.com>
2025-07-17 14:53:07 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Booking from './pages/Booking';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';
+import Admin from './pages/Admin';
+import LiveChat from './components/LiveChat';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+        <Footer />
+        <LiveChat />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
